9bb313b336af3f37cdf4b9812e3daa876028b82c
Mantas Morkūnas <mantas.morkunas@betamedia.lt>
2018-02-07 15:49:47 +0200

    work changes

--- src/Views/Questions/component.jsx
+++ src/Views/Questions/component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Component as Button } from '../../Components/Button'
 import { Component as Input } from '../../Components/Input'
+import dots from '../../Assets/images/dots.png'
 class Questions extends Component {
 	static contextTypes = {
         router: () => null
@@ -8,10 +9,10 @@ class Questions extends Component {
 
 	componentWillMount () {
 
-		if (!this.props.email) {
-			this.context.router.history.push('/')
-			return
-		}
+		// if (!this.props.email) {
+		// 	this.context.router.history.push('/')
+		// 	return
+		// }
 
 		if (this.props.questions.length === 0) {
 			this.props.fetchQuestions()
@@ -20,11 +21,17 @@ class Questions extends Component {
 
     render() {
         return(
-			<div className="app-questions app-container">
-				<h1>{this.props.currentQuestion.question}</h1>
+			<div className="app-questions">
+				<div className="app-questions__header" style={{ backgroundImage: `url(${ dots })` }}>
+					<div className="app-questions__title">Question 1.</div>
+					<h1>{this.props.currentQuestion.question}</h1>
+				</div>
+				<div className="app-questions__progress"></div>
 
-				<Input placeholder="Answer"/>
-				{!this.props.isFetching && <Button text="Next question" onClick={() => this.props.nextQuestion()}/> }
+
+				<div className="app-container">
+					{!this.props.isFetching && <Button text="Next question" onClick={() => this.props.nextQuestion()}/> }
+				</div>
 			</div>
         )
     }

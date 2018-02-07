30540591cd951e8e1eb2eff09061ed4d96b8fd72
Mantas Morkūnas <mantas.morkunas@betamedia.lt>
2018-02-07 02:48:19 +0200

    Heroku setup

--- src/Views/Questions/component.jsx
+++ src/Views/Questions/component.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import { Component as Button } from '../../Components/Button'
 import { Component as Input } from '../../Components/Input'
 class Questions extends Component {
